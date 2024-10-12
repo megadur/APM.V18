159d9f75adbdce18009fe9dbf568a67bb46d938f
Mega Dur <megadur@gmail.com>
2024-10-12 20:58:17 +0200

    With Logging

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,15 +5,20 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './features/products/data/product-data';
+import { provideLogger } from './shared/util-logger';
+import { loggerConfig } from './logger.config';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, 
       withPreloading(PreloadAllModules),
-      withDebugTracing(),),
+    //  withDebugTracing(),
+    ),
     provideHttpClient(),
     importProvidersFrom(InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 })),
+    provideLogger(loggerConfig),
 
 
 //    provideHttpClient(withInterceptors([authInterceptor])),
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LogService } from '../../shared/util-logger';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   public pageTitle = 'Welcome';
+  logger = inject(LogService);
+  constructor() {
+    this.logger.debug('home', 'My Debug Message');    
+    this.logger.info('home', 'My Info Message');    
+    this.logger.error('home', 'My Error Message');   
+  }
 }
